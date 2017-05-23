a0e67b9baf0c8dd95043947e0131eaeeb69af972
Nathaniel Graff <nathaniel.graff@gmail.com>
2017-05-23 15:23:42 -0700

    Raytracer outputs BMP

--- tracer/src/main.rs
+++ tracer/src/main.rs
@@ -1,3 +1,7 @@
+#[macro_use]
+extern crate bmp;
+use bmp::{Image, Pixel};
+
 mod data;
 mod cast;
 
@@ -9,8 +13,8 @@ fn main() {
         eye: Point{x: 0.0, y: 0.0, z: -10.0},
         eye_width: 10.0,
         eye_height: 10.0,
-        img_width: 20,
-        img_height: 20,
+        img_width: 400,
+        img_height: 400,
     };
 
     let s = Scene {
@@ -32,5 +36,23 @@ fn main() {
         light_color: Color{r: 0.0, g: 0.0, b: 0.0},
     };
 
-    print_bitmap(&cast_viewport(&v, &s));
+    let bitmap = cast_viewport(&v, &s);
+    save_bmp(&bitmap, &v);
+}
+
+fn save_bmp(bitmap: &Vec<Vec<Color>>, v: &Viewport) {
+    let mut img = Image::new(v.img_width, v.img_height);
+
+    for (x, y) in img.coordinates() {
+        let xp = x as usize;
+        let yp = y as usize;
+
+        let r = f64::floor(bitmap[xp][yp].r * 255.0) as u8;
+        let g = f64::floor(bitmap[xp][yp].g * 255.0) as u8;
+        let b = f64::floor(bitmap[xp][yp].b * 255.0) as u8;
+
+        img.set_pixel(x, y, px!(r, g, b));
+    }
+
+    let _ = img.save("img.bmp");
 }
